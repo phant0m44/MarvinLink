f76cb450629e3021a3c5e4cadab0bfe79fc8e085
svvvyyat <vitkogame228@gmail.com>
2025-09-01 20:31:19 +0300

    new file:   __pycache__/gptModelOnline.cpython-311.pyc 	modified:   main.py

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from flask import Flask, request
 import os
 #-------------------------------------
 from gptModelOnline import gpt4_ask
-from faster_whisper import WhisperModel
+from google.cloud import speech_v1p1beta1
+#from faster_whisper import WhisperModel
 
 app = Flask(__name__)
+client = speech_v1p1beta1.SpeechClient()
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-model = WhisperModel("medium", device="cuda", compute_type="int8")
+#model = WhisperModel("medium", device="cuda", compute_type="int8")
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -31,12 +33,27 @@ def upload():
 def process_files():
     print("[PROCESSING] Обробка файлів")
 
-    segments, info = model.transcribe("uploads/speech.wav", beam_size=5)
-    text = " ".join([seg.text for seg in segments])
-    print("STT:", text)
-
+    #segments, info = model.transcribe("uploads/speech.wav", beam_size=5)
+    #text = " ".join([seg.text for seg in segments])
+    #print("STT:", text)
+    with open("uploads/modules.txt", "r", encoding="utf-8") as modules_file:
+        txt = modules_file.readline().strip()
+    with open("uploads/speech.wav", "rb") as audio_file:
+        content = audio_file.read()
+    audio = speech_v1p1beta1.RecognitionAudio(content=content)
+    config = speech_v1p1beta1.RecognitionConfig(
+        encoding=speech_v1p1beta1.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code="uk-UA", 
+    )
+    print("[PROCESSING] Відправка аудіо на Google STT...")
+    response = client.recognize(config=config, audio=audio)
+    ask = ''
+    for result in response.results:
+        ask += result.alternatives[0].transcript
+    ask = ask+ '['+txt+']'+'| (Answer simple and don`t use any emojis)'
     #gpt4_ask(f"Привіт, яка зараз температура на кухні?[temp_kitchen: 23; temp_bathroom: 19; temp_outside: 12; localtime: 20:02;] | (Answer simple and don`t use any emojis)")
-
+    gpt4_ask(ask)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
